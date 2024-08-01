0d37ce52fd34720db13a1ab83cca2b12a15c2022
laksby <laksby@icloud.com>
2024-08-02 00:24:56 +0200

    Test mode for AR.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,99 +1,43 @@
-import { FC, useCallback, useRef } from 'react';
+import { FC, useEffect } from 'react';
+import '@ar-js-org/ar.js';
 import * as THREE from 'three';
 import { createBoxBufferGeometry } from '../utils';
 
 export const IndexPage: FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const scene = document.querySelector('a-scene').object3D;
+    const camera = document.querySelector('[camera]').object3D;
 
-  const onActivateClick = useCallback(async () => {
-    try {
-      if (!canvasRef.current || !navigator.xr) {
-        return;
-      }
-      const gl = canvasRef.current.getContext('webgl2', { xrCompatible: true });
+    // const materials = [
+    //   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+    //   new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+    //   new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    //   new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+    //   new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+    //   new THREE.MeshBasicMaterial({ color: 0xffff00 }),
+    // ];
 
-      if (!gl) {
-        return;
-      }
-
-      const scene = new THREE.Scene();
-
-      const materials = [
-        new THREE.MeshBasicMaterial({ color: 0xff0000 }),
-        new THREE.MeshBasicMaterial({ color: 0x0000ff }),
-        new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
-        new THREE.MeshBasicMaterial({ color: 0xff00ff }),
-        new THREE.MeshBasicMaterial({ color: 0x00ffff }),
-        new THREE.MeshBasicMaterial({ color: 0xffff00 }),
-      ];
-
-      // Create the cube and add it to the demo scene.
-      const cube = new THREE.Mesh(createBoxBufferGeometry(0.2, 0.2, 0.2), materials);
-      cube.position.set(1, 1, 1);
-      scene.add(cube);
-
-      const renderer = new THREE.WebGLRenderer({
-        alpha: true,
-        preserveDrawingBuffer: true,
-        canvas: canvasRef.current,
-        context: gl,
-      });
-      renderer.autoClear = false;
-
-      const camera = new THREE.PerspectiveCamera();
-      camera.matrixAutoUpdate = false;
-
-      const session = await navigator.xr.requestSession('immersive-ar');
-      session.updateRenderState({
-        baseLayer: new XRWebGLLayer(session, gl),
-      });
-
-      const referenceSpace = await session.requestReferenceSpace('local');
-
-      const onXRFrame = (_time: DOMHighResTimeStamp, frame: XRFrame) => {
-        session.requestAnimationFrame(onXRFrame);
-
-        if (!session.renderState.baseLayer) {
-          return;
-        }
-
-        gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer);
-
-        const pose = frame.getViewerPose(referenceSpace);
-
-        if (pose) {
-          const view = pose.views[0];
-
-          const viewport = session.renderState.baseLayer.getViewport(view);
-
-          if (!viewport) {
-            return;
-          }
-
-          renderer.setSize(viewport.width, viewport.height);
-
-          camera.matrix.fromArray(view.transform.matrix);
-          camera.projectionMatrix.fromArray(view.projectionMatrix);
-          camera.updateMatrixWorld(true);
-
-          renderer.render(scene, camera);
-        }
-      };
-      session.requestAnimationFrame(onXRFrame);
-    } catch (error) {
-      alert(error);
-    }
+    // const cube = new THREE.Mesh(createBoxBufferGeometry(0.2, 0.2, 0.2), materials);
+    // cube.position.set(1, 1, 1);
+    // scene.add(cube);
   }, []);
 
   return (
-    <main className="tw-p-4 tw-min-h-screen tw-flex tw-flex-col tw-items-stretch">
-      <button
-        onClick={onActivateClick}
-        className="tw-border-2 tw-border-black tw-border-solid tw-p-4 tw-font-bold tw-uppercase tw-self-center hover:tw-bg-black hover:tw-text-white">
-        Start
-      </button>
-      <canvas className="tw-flex-1" ref={canvasRef} />
-    </main>
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+        <a-scene embedded arjs="sourceType: webcam;">
+          <a-marker preset="hiro">
+            <a-entity
+              geometry="primitive: box; width: 0.2; height: 0.2; depth: 0.2"
+              material="color: red"
+              position="0 0.1 0"></a-entity>
+          </a-marker>
+          <a-entity camera></a-entity>
+        </a-scene>
+      `,
+      }}
+    />
   );
 };
 
